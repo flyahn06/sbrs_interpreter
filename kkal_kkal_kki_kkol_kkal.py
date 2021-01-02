8027ceb7c5df0d9cdcd3166ca7362d69686e4b2f
flyahn06 <flyahn06@gmail.com>
2021-01-02 02:19:09 +0000

    추상모듈 기능구현

--- kkal_kkal_kki_kkol_kkal.py
+++ kkal_kkal_kki_kkol_kkal.py
@@ -1,11 +1,11 @@
 import sys
 commandset = ['kkal', 'kki', 'kkol']
 
-def kkal(_from, to):
-    pass
+def kkal(f, t):
+    return 0, f
 
-def kki(_from, to):
-    pass
+def kki(f, t):
+    return f, f+t
 
 def kkol():
     sys.exit()
